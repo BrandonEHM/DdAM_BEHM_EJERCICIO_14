e817f674bb57aa365ab4fb6877864bd6ed311889
BrandonEHM <brandonhermart@gmail.com>
2024-12-09 16:23:08 -0600

    Brandon Enrique Hernandez Martinez

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DirectivasComponent } from "./directivas/directivas.component";
+import { EncabezadoComponent } from "./encabezado/encabezado.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DirectivasComponent, EncabezadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cbdf-behm-ejercicio-14';
+  title = 'cbdf-behm-ejercicio-14 -@if y @for';
 }
